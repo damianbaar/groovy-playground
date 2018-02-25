792b95af92627a02e838508c52ad722d93253a85
Damian Baar <damian.baar@gmail.com>
2018-02-25 16:25:48 +0100

    adding gradle, adding spock

--- src/test/groovy/HelloSpock.groovy
+++ src/test/groovy/HelloSpock.groovy
@@ -1,3 +1,26 @@
+import spock.lang.Specification
 
-class HelloSpock {
+class HelloSpoc extends Specification {
+  def "length of Spock's and his friends' names"() {
+    expect:
+    name.size() == length
+
+    where:
+    name     | length
+    "Spock"  | 5
+    "Kirk"   | 4
+    "Scotty" | 6
+  }
+
+  def "Testing unfold with spock"() {
+    expect:
+      Sth
+        .of(name)
+        .unfold() == result
+
+    where:
+      name | result
+    "test" | "test"
+      null | null
+  }
 }
